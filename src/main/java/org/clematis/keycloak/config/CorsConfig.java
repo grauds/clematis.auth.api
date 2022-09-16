1f73d325245d2af1c20b3b232ff294f38fe7e805
grauds <antroshin@wiley.com>
2022-09-16 18:24:32 +0300

    [+] Fixed checkstyle

--- src/main/java/org/clematis/keycloak/config/CorsConfig.java
+++ src/main/java/org/clematis/keycloak/config/CorsConfig.java
@@ -1,13 +1,13 @@
 package org.clematis.keycloak.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 /**
  * @author Ruslan Lagay
