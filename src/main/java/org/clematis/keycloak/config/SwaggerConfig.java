08aa251ebeb2d04014267a5608470dfdd183c6e9
grauds <antroshin@wiley.com>
2022-12-22 11:44:01 +0300

    [+] Upgraded spring boot version

--- src/main/java/org/clematis/keycloak/config/SwaggerConfig.java
+++ src/main/java/org/clematis/keycloak/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package org.clematis.keycloak.config;
 
 import org.springdoc.core.GroupedOpenApi;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 /**
  *
  * @author Anton Troshin
@@ -11,13 +14,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-    public SwaggerConfig() {
+    private final BuildProperties buildProperties;
+
+    public SwaggerConfig(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(getInfo());
+    }
+
+    public Info getInfo() {
+        return new Info().title("Clematis Keycloak Auth API")
+                .description("")
+                .version(buildProperties.getVersion())
+                .description(buildProperties.getName());
     }
 
     @Bean
     public GroupedOpenApi api() {
         return GroupedOpenApi.builder()
-                .group("Clematis-Auth-API")
+                .group("Authentication")
                 .pathsToMatch("/**")
                 .build();
     }
