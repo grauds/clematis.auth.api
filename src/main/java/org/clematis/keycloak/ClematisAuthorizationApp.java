713250174d32a7c1104e95fd4146bab7a4250191
grauds <antroshin@wiley.com>
2022-12-22 13:32:09 +0300

    [+] Fixed runtime deps

--- src/main/java/org/clematis/keycloak/ClematisAuthorizationApp.java
+++ src/main/java/org/clematis/keycloak/ClematisAuthorizationApp.java
@@ -4,10 +4,15 @@ import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 /**
  * @author Anton Troshin
  */
-@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class, proxyBeanMethods = false)
+@SpringBootApplication(exclude = {
+        LiquibaseAutoConfiguration.class,
+        SecurityAutoConfiguration.class
+    }, proxyBeanMethods = false)
 public class ClematisAuthorizationApp {
     public static void main(String[] args){
         BasicConfigurator.configure();
