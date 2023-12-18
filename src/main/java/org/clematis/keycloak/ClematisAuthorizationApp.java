8e98e556a9eea77b5a5a1979dbec1ac94d4fe094
Anton Troshin <11165918+grauds@users.noreply.github.com>
2023-12-18 14:33:26 +0300

    [+] Migrated to Keycloak 20

--- src/main/java/org/clematis/keycloak/ClematisAuthorizationApp.java
+++ src/main/java/org/clematis/keycloak/ClematisAuthorizationApp.java
@@ -1,6 +1,5 @@
 package org.clematis.keycloak;
 
-import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
@@ -15,7 +14,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
     }, proxyBeanMethods = false)
 public class ClematisAuthorizationApp {
     public static void main(String[] args){
-        BasicConfigurator.configure();
         SpringApplication.run(ClematisAuthorizationApp.class, args);
     }
 }
